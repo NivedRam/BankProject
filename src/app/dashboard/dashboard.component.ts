9f4a97d48f249e12ae48ac93c0a27ec36a95a09b
NivedRam <115545444+NivedRam@users.noreply.github.com>
2022-12-09 13:46:38 +0530

    Seccond Commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+acno=''
+psw=''
+amnt=''
+acno1=''
+psw1=''
+amnt1=''
+user=''
+constructor(private ds:DataService){
+//Access user name
+this.user=this.ds.currentuser
+
+
+}
+deposit(){
+  var acno=this.acno
+  var psw=this.psw
+  var amnt=this.amnt
+  const result=this.ds.deposit(acno,psw,amnt)
+  if(result){
+    alert(`${amnt} credited to you account and the available balance is ${result}`)
+  }
+  else{
+    alert("incurrect username or password")
+  }
+
+}
+wihtdraw(){
+  var acno1=this.acno1
+  var psw1=this.psw1
+  var amnt1=this.amnt1
+
+  const result=this.ds.withdraw(acno1,psw1,amnt1)
+  if(result){
+    alert(`${amnt1} debited from your account and the available balance is ${result}`)
+  }
+
+
+}
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -10,53 +12,44 @@ export class LoginComponent {
   Data="Enter Account No."
   acno=''
   psw=''
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:123,balance:0},
-    1001:{acno:1001,username:"arun",password:123,balance:0},
-    1002:{acno:1003,username:"amal",password:123,balance:0},
-    1003:{acno:1003,username:"mega",password:123,balance:0}
+ 
+  constructor(private router:Router,private ds:DataService){}
+  
+  login(){
+  
+    
+    // alert('login clicked')
+   var acno=this.acno
+   var psw=this.psw
+
+   const result=this.ds.login(acno,psw)
+   if(result){
+    alert('login success')
+    this.router.navigateByUrl('dashboard')
+   }
+   else{
+    alert('incurect user name or password')
+   }
   }
-  // login(){
-  //   // alert('login clicked')
-  //  var acno=this.acno
-  //  var psw=this.psw
-  //  var userDetails=this.userDetails
-
-  //  if(acno in userDetails){
-  //   if(psw==userDetails[acno]["password"]){
-  //   alert("login success")}
-  //   else{
-  //     alert("incurrect password")
-  //   }
-  //  }
-  //  else{
-  //   alert("user does not exist")
-  //  }
-  // }
-//   acnoChange(event:any){
-// this.acno=event.target.value
-//   }
-//   pswChange(event:any){
-//     this.psw=event.target.value
-//   }
-
-login(a:any,b:any){
-  this.acno=a.value
-  this.psw=b.value
-
- var acno=this.acno
- var psw=this.psw
- var userDetails=this.userDetails
-
- if(acno in userDetails){
-  if(psw==userDetails[acno]["password"]){
-  alert("login success")}
-  else{
-    alert("incurrect password")
-  }
- }
- else{
-  alert("user does not exist")
- }
 }
-}
+
+// login(a:any,b:any){
+//   this.acno=a.value
+//   this.psw=b.value
+
+//  var acno=this.acno
+//  var psw=this.psw
+//  var userDetails=this.userDetails
+
+//  if(acno in userDetails){
+//   if(psw==userDetails[acno]["password"]){
+//   alert("login success")}
+//   else{
+//     alert("incurrect password")
+//   }
+//  }
+//  else{
+//   alert("user does not exist")
+//  }
+// }
+// }
