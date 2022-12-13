33000b1d44da0f40f5e1ab29e5148e7dba1bb9fb
NivedRam <115545444+NivedRam@users.noreply.github.com>
2022-12-13 13:44:00 +0530

    third

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -7,23 +8,26 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-acno=''
-psw=''
-amnt=''
-acno1=''
-psw1=''
-amnt1=''
+// acno=''
+// psw=''
+// amnt=''
+// acno1=''
+// psw1=''
+// amnt1=''
 user=''
-constructor(private ds:DataService){
+constructor(private ds:DataService,private fb:FormBuilder){
 //Access user name
 this.user=this.ds.currentuser
 
 
 }
+
+depositForm=this.fb.group({acno:['',[Validators.required,Validators.pattern('[0-9]')]],psw:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]')]],amnt:['',[Validators.required,Validators.pattern('[0-9]')]]})
+withdrawForm=this.fb.group({acno1:['',[Validators.required,Validators.pattern('[0-9]')]],psw1:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]')]],amnt1:['',[Validators.pattern('[0-9]')]]})
 deposit(){
-  var acno=this.acno
-  var psw=this.psw
-  var amnt=this.amnt
+  var acno=this.depositForm.value.acno
+  var psw=this.depositForm.value.psw
+  var amnt=this.depositForm.value.amnt
   const result=this.ds.deposit(acno,psw,amnt)
   if(result){
     alert(`${amnt} credited to you account and the available balance is ${result}`)
@@ -34,9 +38,9 @@ deposit(){
 
 }
 wihtdraw(){
-  var acno1=this.acno1
-  var psw1=this.psw1
-  var amnt1=this.amnt1
+  var acno1=this.withdrawForm.value.acno1
+  var psw1=this.withdrawForm.value.psw1
+  var amnt1=this.withdrawForm.value.amnt1
 
   const result=this.ds.withdraw(acno1,psw1,amnt1)
   if(result){
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -8,28 +9,36 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  aim="Welcome To The World Of New Generation Banking"
-  Data="Enter Account No."
-  acno=''
-  psw=''
- 
-  constructor(private router:Router,private ds:DataService){}
-  
-  login(){
-  
-    
-    // alert('login clicked')
-   var acno=this.acno
-   var psw=this.psw
+  aim = "Welcome To The World Of New Generation Banking"
+  Data = "Enter Account No."
+  // acno=''
+  // psw=''
 
-   const result=this.ds.login(acno,psw)
-   if(result){
-    alert('login success')
-    this.router.navigateByUrl('dashboard')
-   }
-   else{
-    alert('incurect user name or password')
-   }
+  constructor(private router: Router, private ds: DataService, private fb: FormBuilder) { }
+
+
+  loginForm = this.fb.group({ acno: ['', [Validators.required, Validators.pattern('[0-9]+')]], psw: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]] })
+
+  login() {
+
+
+    // alert('login clicked')
+    var acno = this.loginForm.value.acno
+    var psw = this.loginForm.value.psw
+
+    const result = this.ds.login(acno, psw)
+    if (this.loginForm.valid) {
+      if (result) {
+        alert('login success')
+        this.router.navigateByUrl('dashboard')
+      }
+      else {
+        alert('incurect user name or password')
+      }
+    }
+    else{
+      alert('invalid form')
+    }
   }
 }
 
